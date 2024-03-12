c15fcb7c55153eb3f655bd45b7199ba1fcfda10f
Shaswath <shaswath.g@kalvium.community>
2024-03-12 14:49:34 +0530

    added try and catch for connection

--- server.js
+++ server.js
@@ -1,40 +1,43 @@
 const express = require('express');
 require("dotenv").config();
-const mongoose = require('mongoose')
-const {Connect, isConnected} = require("./db")
-const cors = require('cors')
-const bodyParser = require('body-parser')
-
+const mongoose = require('mongoose');
+const { Connect, isConnected } = require("./db");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
 
-//Connecting to MongoDB
+// Connect to MongoDB
 Connect()
-
+    .then(() => {
+        console.log("Connected to MongoDB");
+        // Start the server after successful connection
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`Server running on port ${process.env.PORT || 3000}`);
+        });
+    }) .catch((error) => {
+        console.error("Failed to connect to MongoDB:", error.message);
+        process.exit(1); 
+    });
 
 app.get("/", (req, res) => {
-    const htmlResponse = `<h1><i>Expense Manager</i></h1><p>Database Connection Status: ${isConnected ? 'Connected':'Disconnected'}</p>`;
-    res.send(htmlResponse);
+  const htmlResponse = `<h1><i>Expense Manager</i></h1><p>Database Connection Status: ${isConnected ? 'Connected' : 'Disconnected'}</p>`;
+  res.send(htmlResponse);
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
+
+
 
 mongoose.connection.once('open', () => {
     console.log("Connected to mongoDB");
-    if (require.main === module) {
-        app.listen(process.env.PORT || 3000, () => {
-            console.log(`Server running on port ${process.env.PORT || 3000}`);
-        });
-    } else {
-        console.log("error");
-    }
 });
 
 module.exports = app;
